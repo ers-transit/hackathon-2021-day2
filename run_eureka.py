07c6bebca87a8ddacfc1d38d38ed0df4af5ead8b
Laura Kreidberg <kreidberg@mpia.de>
2021-06-16 17:08:47 +0200

    update function calls

--- run_eureka.py
+++ run_eureka.py
@@ -6,7 +6,7 @@ import eureka.S4_generate_lightcurves.s4_genLC as s4
 eventlabel = 'wasp43b'
 
 reload(s3)
-ev3 = s3.reduceJWST(eventlabel, isplots=0, testing=False)
+ev3 = s3.reduceJWST(eventlabel)
 
 reload(s4)
-ev4 = s4.lcJWST(ev3.eventlabel, ev3.workdir, md=ev3, isplots=0)
+ev4 = s4.lcJWST(ev3.eventlabel, ev3.workdir, md=ev3)
